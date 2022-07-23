c1c341e93deb27a225ba65f9bb21ad8f910c8abc
NavisAzka <navisazka26@gmail.com>
2022-07-23 09:56:09 +0700

    First commit, empty project and .gitignore

---
+++ Assets/LinesSc.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LinesSc : MonoBehaviour
+{
+    [SerializeField] InputField inputAngle;
+
+    [SerializeField] LineRenderer line;
+    [SerializeField] Transform pointStart;
+    [SerializeField] Transform pointEnd;
+
+    [SerializeField] Trigono sin;
+    [SerializeField] Trigono cos;
+    [SerializeField] Trigono tan;
+
+    public float ag;
+
+    public int angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetLine(line, pointStart.position, pointEnd.position);
+    }
+
+    public void SetAngles()
+    {
+        string isi = "";
+
+        if (inputAngle.text == "" || inputAngle.text == null)
+        {
+            isi = "0";
+        }
+        else
+        {
+            isi = inputAngle.text;
+        }
+
+        angle = int.Parse(isi);
+
+        transform.GetChild(0).rotation = Quaternion.Euler(0, 0, angle);
+
+        SetLine(line, pointStart.position, pointEnd.position);
+        SetLine(sin.line, pointStart.position, new Vector2(pointStart.position.x, pointEnd.position.y));
+        SetLine(cos.line, pointStart.position, new Vector2(0, pointStart.position.y));
+
+        sin.result.text = "Sin : " + Mathf.Sin(ag * Mathf.Deg2Rad).ToString();
+        cos.result.text = "Cos : " + Mathf.Cos(ag * Mathf.Deg2Rad).ToString();
+    }
+
+    private void Update()
+    {
+    }
+
+    void SetLine (LineRenderer _line, Vector2 posS, Vector2 posE)
+    {
+        _line.SetPosition(0, posS);
+        _line.SetPosition(1, posE);
+    }
+
+
+}
---
+++ Assets/Trigono.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigono : MonoBehaviour
+{
+    public Text result;
+
+    public LineRenderer line;
+    public Transform pointStart;
+    public Transform pointEnd;
+}
